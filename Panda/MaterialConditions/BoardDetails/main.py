b102a66849e9db85eacc6fb0418b0af5e958cdcf
serpe <vladvladserpe@gmail.com>
2025-12-16 16:28:10 +0200

    Updated logic and repaired king moves

--- Panda/MaterialConditions/BoardDetails/main.py
+++ Panda/MaterialConditions/BoardDetails/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5178a92028979cff2d2d27249cacec04964808685137ba6e2eb1c6380c0093f
-size 2102
+oid sha256:e7c1a4cc6e2e0885712ac5689ecd153418143ffa7ebbf984a018a0c16f5f5eec
+size 2365
--- Panda/MaterialConditions/BoardDetails/pieces.py
+++ Panda/MaterialConditions/BoardDetails/pieces.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:293ceb5799c5e4fd38129d6e3f99d9c3ab0ba54cd58d764f2560237aa93c9f95
-size 11312
+oid sha256:60baf64cc307bca3aeb4ded7cfa465c483c4aa54821b8efe3de8b77624eb7dbd
+size 11409
